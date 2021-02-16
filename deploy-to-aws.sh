440b37b0914cb478717567ce165a8b65d0017d1d
usmanovbf <usmanovbf@gmail.com>
2021-02-16 15:21:29 +0300

    adopted for UpTeam company

---
+++ deploy-to-aws.sh
@@ -0,0 +1,5 @@
+npm run deploy -- --region eu-west-3 --stage digito
+npm run deploy -- --region ap-southeast-1 --stage digito
+npm run deploy -- --region eu-central-1 --stage digito
+npm run deploy -- --region eu-west-1 --stage digito
+npm run deploy -- --region eu-west-2 --stage digito
